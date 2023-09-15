f3ba2597084e8a0c58ef39829343c61d6df0ba15
ralph <sanghwaj222@gmail.com>
2023-09-15 17:15:30 +0900

    230915 swift

---
+++ PS_Swift/BOJ10926.swift
@@ -0,0 +1,11 @@
+//
+//  BOJ10926.swift
+//  PS_Swift
+//
+//  Created by 정상화 on 2023/09/11.
+//
+import Foundation
+func gkatn() {
+    let ID = readLine()!
+    print(ID + "??!") //변수 ID와 문자열을 출력하려면 print함수 내에서 + 연산자를 사용해야 한다.
+}
---
+++ PS_Swift/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  PS_Swift
+//
+//  Created by 정상화 on 2023/09/11.
+//
+import Foundation
+gkatn()
