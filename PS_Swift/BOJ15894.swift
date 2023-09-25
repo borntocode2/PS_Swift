d545ca3d23905d6062e9bf868c945fcd48b04eb7
ralph <sanghwaj222@gmail.com>
2023-09-25 18:32:48 +0900

    230925 Swift

---
+++ PS_Swift/BOJ15894.swift
@@ -0,0 +1,13 @@
+//
+//  BOJ15894.swift
+//  PS_Swift
+//
+//  Created by 정상화 on 2023/09/25.
+//
+
+import Foundation
+func BOJ15894(){
+    let input = Int(readLine()!)!
+    print(input * 4)
+    
+}
