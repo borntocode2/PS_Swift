857a4782b4ab8a71dc076bc1f23bdbd5c1ea89b4
ralph <sanghwaj222@gmail.com>
2023-09-26 20:29:09 +0900

    230926 Optional

---
+++ PS_Swift/about Optional.swift
@@ -0,0 +1,23 @@
+//
+//  about Optional.swift
+//  PS_Swift
+//
+//  Created by 정상화 on 2023/09/26.
+//
+//
+//why optional dose exist
+//there are nil and value that not nil
+//
+//if optional dosen't exist{
+//    can't not return nil, occurs an error
+//    program have a huse code.
+//    it can't be stop because of a few error.
+//    so, first of all, there is optional type.
+//    if there is an error, can return nil because of optional type.
+//        ex) Int("hi") "hi" is string not Int.
+//        so, it will occur an error.
+//        Swift wants not error,
+//    so we have to unlock optional types.
+//}
+
+
