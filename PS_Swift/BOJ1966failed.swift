c693a07499e08a537ed51ed0f6a601b69cb3a9b9
ralph <sanghwaj222@gmail.com>
2023-10-09 16:24:01 +0900

    231009 Swift

--- PS_Swift/BOJ1966failed.swift
+++ PS_Swift/BOJ1966failed.swift
@@ -13,23 +13,40 @@ func BOJ1966(){
         let arr = docu_num.split(separator:" ")
         let docu = Int(arr[0])!   // docu의 수
         let impor = Int(arr[1])!  // 순서를 출력할 문서의 순서
-        var impor_docu = readLine()! // 중요도 문서들
+        let input = readLine()! // 중요도 문서들
+        var impor_docu = input.split(separator:" ")
         var compare = [String]()
-        for i in 0...docu - 1{
-            if i == impor - 1{
+        
+        for i in 0...docu - 1 { // compare에 우리가 추적해야할 impor문서를 T로 넣어준다.
+            if  i == impor{
                 compare.append("T")
             }
             else{
                 compare.append("F")
             }
         }
-        
-        
+        var count = 0
+        let k = 0
+        for i in 0..<docu{
+            var count = 0
+            for j in 1..<docu{
+                if Int(impor_docu[i])! < Int(impor_docu[j])!{
+                    count = 1
+                }
+            }
+            if count == 1{
+                impor_docu.append(impor_docu.removeFirst())
+                compare.append(compare.removeFirst())
+            }
+            print(impor_docu)
+        }
+        print(compare.firstIndex(of:"T")! + 1)
     }
+
+    // 1 2 3 4
+    // 2 3 4 1
+    // 3 4 1 2
+    // 4 1 2 3
+    // 1 2 3 4
+    //
 }
-// 4 2 1 3
-// 2 1 3
-// 1 3 2
-// 3 2 1
-// 2 1
-// 1
