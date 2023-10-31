2240c44a5af56b1fd2a05b26beb108dcb4fbe9bf
ralph <sanghwaj222@gmail.com>
2023-10-31 21:46:14 +0900

    231031 BFS-ing

--- PS_Swift/BOJ2468그래프.swift
+++ PS_Swift/BOJ2468그래프.swift
@@ -12,7 +12,7 @@ func BOJ2468(){
     var my_map = [[Int]]() //map으로 저장하려면 2차원배열로 선언해준다.
     let dx = [0, -1, 0, 1]
     let dy = [-1, 0, 1, 0]
-    var dq = [Int]()
+    var dq = [(Int, Int)]() // 좌표 append할 수 있는 배열선언
     for _ in 1...N{
         var input = readLine()!
         my_map.append(input.split(separator:" ").compactMap { Int($0)}) //input받은 것을 2차원배열my_map에 append
@@ -24,22 +24,29 @@ func BOJ2468(){
                     visits[i][j] = 1
                 }
                 if visits[i][j] == -1{
-                    dq.append(contentsOf: [i, j]) // dq.append([i, j])는 [i, j] 가 Int타입이 아니라 하나의 배열이기 때문
+                    dq.append((i, j)) // 선언 -> var dq = [(Int, Int)] / dq.append((i,j))
                 }
             }
         }
         while !(dq.isEmpty){
-            var dq_cas = dq[dq.indices]
-            var coord = dq_cas.popFirst()!
-            var x = Int(coord[0])!
-            var y = coord[1]
-            if
+            let (x, y) = dq.removeFirst()
+            for i in 0..<3{
+                var nx = dx[i] + x
+                var ny = dy[i] + y
+                if nx >= 0 && ny >= 0 && nx < N && ny < N{
+                    if visits[nx][ny] == -1{
+                        
+                    }
+                }
             }
+            }
+        
+        
         }
             
         }
         
-    }
+    
     
     
     
--- PS_Swift/test.swift
+++ PS_Swift/test.swift
@@ -4,5 +4,9 @@
 //
 //  Created by 정상화 on 2023/09/15.
 //
-
-
+func test(){
+    var dq = [(Int,Int)]()
+    dq.append((1, 2))
+    let (x, y) = dq.removeFirst()
+    print((x, y))
+}
