4e5a18beb00c103f59594bbd4d1a5136bd18c5de
ralph <sanghwaj222@gmail.com>
2023-11-08 20:03:11 +0900

    231108 Swift BFS -ing

--- PS_Swift/BOJ24444BFS.swift
+++ PS_Swift/BOJ24444BFS.swift
@@ -13,8 +13,9 @@ func BOJ24444(){
     let N = arr[0] // 정점의 수
     let M = arr[1] // 간선의 개수
     let R = arr[2] // 시작지점
-    var relations = [[Int]](repeating: [], count: N) // 정점의 수 만큼 relations에 배열을 생성
-    var visits = [Int](repeating: -1, count: N) // 정점의 수 만큼 visits에 0을 생성
+    var relations = [[Int]](repeating: [], count: N+1) // 정점의 수 만큼 relations에 배열을 생성
+    var visits = [Int](repeating: -1, count: N+1) // 정점의 수 만큼 visits에 0을 생성
+    var cnt_rela = [Int](repeating: 0, count: N+1)
     
     for i in 0..<M{ // 양방향 간선에 따라 relations 정보 저장
         var imfo = readLine()!
@@ -22,6 +23,30 @@ func BOJ24444(){
         relations[imfo_split[0]].append(imfo_split[1])
         relations[imfo_split[1]].append(imfo_split[0])
     }
+    for i in 0..<N{
+        relations[i].sort()
+    }
+    for i in relations[R]{ //dq에 시작지점인 relaitons[R]의 모든 원소를 append
+        dq.append(i)
+        print(i)
+    }
+    visits[R] = 1
+    cnt_rela[R] = 1
+    var cnt = 1
+    
+    while !dq.isEmpty{
+        for i in relations[dq.removeFirst()]{
+            if visits[i] == 0{
+                dq.append(i)
+                
+            }
+            
+            
+            
+        }
+    }
+    
+    
     
     
 }
